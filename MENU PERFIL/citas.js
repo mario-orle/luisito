633d1fe659bca08fa48c047cb42925b63df6fbbb
pablo-89 <trobatela@gmail.com>
2020-12-23 19:25:13 +0100

    lol

--- MENU PERFIL/citas.js
+++ MENU PERFIL/citas.js
@@ -1,14 +1,16 @@
 
 
-var fechas = [new Date(2020, 11, 12), new Date(2020, 11, 13), new Date(2020, 11, 14), new Date(2020, 11, 18)];
+var fechas = [new Date(2020, 11, 12), new Date(2020, 11, 13), new Date(2020, 11, 14),new Date(2020, 11, 15), new Date(2020, 11, 18)];
 
 
 
 const elem = document.getElementById('foo');
 const datepicker = new Datepicker(elem, {
-  maxNumberOfDates: 5
+  maxNumberOfDates: 1,
+  weekStart: 1,
+  language: 'es'
   // ...options
-}); 
+});
 
 datepicker.setDate( ...fechas );
 
@@ -16,6 +18,6 @@ const containerCitasProgramadas = document.getElementById("citas-programadas");
 
 fechas.map(fecha => {
   
-  containerCitasProgramadas.innerHTML += "<li><a>" + fecha.getDate() + "/" + (fecha.getMonth() + 1) + "</a> Bla bla bla</li>";;
+  containerCitasProgramadas.innerHTML += "<li><a>" + fecha.getDate() + "/" + (fecha.getMonth() + 1) + "</a> 12-DICIEMBRE 18:30 JOSECARLOS</li>";;
 
 });
