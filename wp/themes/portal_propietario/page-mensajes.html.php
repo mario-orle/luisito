d9cc1a2685ec48f7d814b5b0c8c81ae9040d6239
Mario <mario.ortas@outlook.com>
2021-01-27 01:58:28 +0100

    chat layout

--- wp/themes/portal_propietario/page-mensajes.html.php
+++ wp/themes/portal_propietario/page-mensajes.html.php
@@ -19,8 +19,42 @@ get_header();
 
 <main id="primary" class="site-main">
     <div class="main">
-        <div class="iframe">
-            <iframe src="white_chat.html"></iframe>
+        <div class="chat">
+            <div class="contactos">
+                <?php
+for ($i=0; $i<3; $i++) {
+                ?>
+                <div class="contacto">
+                    <img class="contacto-img" src="<?php echo get_template_directory_uri() . '/assets/img/'?>perfil.png" />
+                    <div class="contacto-name">Ramón el vanidoso</div>
+                    <div class="contacto-unread">3</div>
+                </div>
+                <?php
+}
+                ?>
+            </div>
+            <div class="mensajes-enviar">
+                <div class="mensajes">
+                    <?php
+for ($i=0; $i<3; $i++) {
+                    ?>
+                    <div class="mensaje">
+                        <div class="author">
+                            <img class="author-img" src="<?php echo get_template_directory_uri() . '/assets/img/'?>perfil.png" />
+                            <div class="author-name">Ramón el vanidoso</div>
+                        </div>
+                        <div class="mensaje-txt">bla bla bla bla bla bla bla</div>
+                    </div>
+                    <?php
+}
+                    ?>
+
+                </div>
+                <div class="enviar">
+                    <textarea></textarea>
+                    <button>Enviar</button>
+                </div>
+            </div>
         </div>
     </div>
 </main><!-- #main -->
