31f0ddea817d90ba06e918efc19bba87457bec30
pablo-89 <trobatela@gmail.com>
2021-02-02 12:12:37 +0100

    cambios

---
+++ MENU PERFIL/version admin/citas-admin.js
@@ -0,0 +1,28 @@
+
+
+var fechas = [new Date(2020, 11, 12), new Date(2020, 11, 13), new Date(2020, 11, 14),new Date(2020, 11, 15), new Date(2020, 11, 18)];
+
+
+
+const elem = document.getElementById('foo');
+const datepicker = new Datepicker(elem, {
+  maxNumberOfDates: 1,
+  weekStart: 1,
+  language: 'es'
+  // ...options
+});
+
+datepicker.setDate( ...fechas );
+
+const containerCitasProgramadas = document.getElementById("citas-programadas");
+
+fechas.map(fecha => {
+  
+  containerCitasProgramadas.innerHTML += "<li><a>" + fecha.getDate() + "/" + (fecha.getMonth() + 1) + "</a> 12-DICIEMBRE 18:30 JOSECARLOS</li>";;
+
+});
+
+function showPopup() {
+  document.getElementById("popup").classList.add("active")
+  document.getElementById("popup-bg").classList.add("active")
+}
