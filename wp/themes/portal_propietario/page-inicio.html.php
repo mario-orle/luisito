e64fc925254c6cec7ffb952824d37c7a72ad91e1
pablo-89 <trobatela@gmail.com>
2021-02-03 13:43:05 +0100

    alot

--- wp/themes/portal_propietario/page-inicio.html.php
+++ wp/themes/portal_propietario/page-inicio.html.php
@@ -26,43 +26,43 @@ get_header();
                 <div class="estadisticas">
                     <div class="visualizaciones">
                         <button>
-                        <a href="#"></a>
+                        <a href="citas-admin.html">
                         <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>lupa.png" width="100%">
                         <h2>Ofertas Recibidas</h2>
                         <p>3 Ofertas</p>
+                        </a>
                         </button>
                     </div>
                     <div class="contacto-email">
                         <button>
-                        <a href="#"></a>
+                        <a href="#">
                         <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>email2.png" width="100%">
                         <h2>Mensajes sin Leer</h2>
                         <p>8 Mensajes Sin Leer</p>
+                        </a>
                         </button>
                     </div>
                     <div class="calendario">
                         <button>
-                        <a href="#"></a>
+                        <a href="#">
                         <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>schedule.png" width="100%">
                         <h2>Citas por Confirmar</h2>
                         <p>3 Citas sin Confirmar</p>
+                        </a>
                         </button>
                     </div>
                     <div class="citas">
                         <button>
-                        <a href="citas.html"></a>
+                        <a href="citas.html">
                         <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>perfil.png" width="100%">
                         <h2>Doc Requeridos</h2>
                         <p>3 Doc Requeridos</p>
+                        </a>
                         </button>
                     </div>
                 </div>
             </div>
         
-
-
-
-
 
         <div class="main-container-sub">
             <h2>Evolución de la Vivienda</h2>
@@ -87,6 +87,52 @@ get_header();
             </div>
         </div>
     </div>
+    <!-- menu admin -->
+    <div class="main">
+                <div class="main-container">
+                    <h2>Estadisticas Generales</h2>
+                        <br>
+                        <div class="estadisticas">
+                                <div class="doc-pendientes">
+                                    <button>
+                                    <a href="#">
+                                        <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>schedule.png" width="100%">
+                                    <h2>Doc Pendientes</h2>
+                                        <p>7 Documentos</p>
+                                        </a>
+                                    </button>
+                                </div>
+                                <div class="doc-revisar">
+                                    <button>
+                                    <a href="#">
+                                        <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>schedule.png" width="100%">
+                                    <h2>Doc Revisar</h2>
+                                    <p>2 Documentos</p>
+                                    </a>
+                                    </button>
+                                </div>
+                                <div class="chat-pendientes">
+                                    <button>
+                                    <a href="#">
+                                        <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>schedule.png" width="100%">
+                                    <h2>Chat Pendientes</h2>
+                                    <p>4 Mensajes sin leer</p>
+                                    </a>
+                                    </button>
+                                </div>
+                                <div class="citas">
+                                    <button>
+                                    <a href="#">
+                                        <img src="<?php echo get_template_directory_uri() . '/assets/img/'?>schedule.png" width="100%">
+                                    <h2>Citas Sin Actualizar</h2>
+                                    <p>6 Citas pendientes</p>
+                                    </a>
+                                    </button>
+                                </div>
+                            </div>
+                        </diV>
+                     </div>
+            <!-- menu arriba admin -->
 </main><!-- #main -->
 
 <?php
