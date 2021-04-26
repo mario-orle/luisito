9a12274ce3599f8a4014e8b6b19168e9d4710ba4
pablo-89 <trobatela@gmail.com>
2021-04-26 13:08:32 +0200

    cambios mobile

---
+++ MENU PERFIL/mobile/select-perfil-inmuebles.js
@@ -0,0 +1,17 @@
+window.initChoices = function () {
+    var choicesObjs = document.querySelectorAll('.js-choice,.js-choices');
+    var choices = [];
+    for (var i = 0; i < choicesObjs.length; i++) {
+      choices.push(new Choices(choicesObjs[i], {
+        itemSelectText: 'Click para seleccionar',
+        searchEnabled: false,
+        shouldSort: false,
+        position: 'bottom'
+      }));
+    }
+    window.choicesObjs = choices;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    window.initChoices();
+  }, false);
